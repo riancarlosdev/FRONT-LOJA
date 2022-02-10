2c7f52286154d5d1ebeae10d85a7972fc5d76927
riancarlosdev <riancontatoprofissional@gmail.com>
2022-02-10 08:19:42 -0300

    concertando posicao da qnt Bag

--- components/1-header/bag/info-qnt-bag.tsx
+++ components/1-header/bag/info-qnt-bag.tsx
@@ -3,7 +3,7 @@ import { infoQntBag_type } from "../../../types";
 export default function InfoQntBag_component({qnt}:infoQntBag_type): JSX.Element {
   return (
     <div className="absolute -top-2 -right-1 w-5 h-5 rounded-full bg-1 flex justify-center items-center">
-      <span className="text-text-1 text-sm font-bold">{qnt}</span>
+      <span className="text-text-1 text-sm font-bold pb-1">{qnt}</span>
     </div>
   )
 }
