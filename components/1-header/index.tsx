c59441605fce8f00cd9cf2f55ea428ec9ace47ad
riancarlosdev <riancontatoprofissional@gmail.com>
2022-02-10 05:39:22 -0300

    header input 100%

--- components/1-header/index.tsx
+++ components/1-header/index.tsx
@@ -1,9 +1,14 @@
 import Logo1 from "../temp/logos/1";
+import InputSearch from "./input-search";
 
 export default function HeaderComponent():JSX.Element {
   return (
     <div className="h-24 flex items-center">
-      <Logo1 />
+      <div>
+        <Logo1 />
+      </div>
+
+      <InputSearch />
     </div>
   )
 }
---
+++ components/1-header/input-search.tsx
@@ -0,0 +1,16 @@
+import { BsSearch } from 'react-icons/bs'
+
+export default function InputSearch():JSX.Element {
+  return (
+    <div className='w-full'>
+      <form className=''>
+        <div className='relative overflow-hidden rounded-full'>
+          <input id='search' type="text" maxLength={50} className='text-lg text-text-2 outline-none placeholder:text-lg pl-7 pr-20 w-full h-12 flex' placeholder="Buscar" />
+          <button type='submit' className='px-4 h-full absolute top-0 right-0'>
+            <BsSearch className='fill-1' size={22} />
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
