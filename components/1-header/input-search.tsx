64e3c441fc5f47fcbd6ea04e8a3535ffbf6a3e3d
riancarlosdev <riancontatoprofissional@gmail.com>
2022-02-10 10:41:40 -0300

    input search colaps

--- components/1-header/input-search.tsx
+++ components/1-header/input-search.tsx
@@ -1,13 +1,19 @@
 import { FormEvent, useEffect, useState } from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { useSelector } from 'react-redux'
 
 export default function InputSearch_component():JSX.Element {
 
-  const [ stateInput, setStateInput ] = useState('text-lg text-text-2 outline-none placeholder:opacity-0 md:placeholder:text-lg pl-7 pr-20 w-full h-12')
   const [ focus, setFocus ] = useState<boolean>(false)
 
   const handleFocus = () => setFocus(!focus)
-  
+
+  const sizeScreen = useSelector(states => states)
+
+  useEffect(() => {
+
+  }, [ sizeScreen ])
+
   function handleClick() {
     const responsive:number = window.innerWidth
 
@@ -25,12 +31,18 @@ export default function InputSearch_component():JSX.Element {
   }
 
   return (
-    <div className='w-full md:block flex justify-end'>
-      <form onSubmit={handleSubmit} name='form' id='form' className=''>
+    <div className={`${sizeScreen > 768 ? 'w-full' : 'w-14'} ease-in-out duration-700`}>
+      <form onSubmit={handleSubmit} name='form' id='form'>
         <div className='relative overflow-hidden rounded-full'>
-          <input id='search' onFocus={handleFocus} onBlur={handleFocus} type="text" maxLength={50} className={stateInput} />
-          <span className='absolute top-3 left-7 text-text-2' hidden={focus}>Buscar</span>
-          <button type='submit' form='form' className='px-4 bg-3 md:bg-opacity-0 h-12 md:h-full absolute top-0 right-0'>
+          <input className={`text-lg text-text-2 outline-none md:placeholder:text-lg pl-7 pr-20 w-full h-12`} 
+            id='search' 
+            type="text" 
+            maxLength={50} 
+            onBlur={handleFocus} 
+            onFocus={handleFocus} 
+          />
+          <span className='absolute top-3 left-7 text-text-2' hidden={sizeScreen > 768 ? focus : true}>Buscar</span>
+          <button onClick={handleFocus} type='submit' form='form' className='px-4 bg-3 md:bg-opacity-0 h-12 md:h-full absolute top-0 right-0'>
             <BsSearch className='fill-text-2 md:fill-text-2' size={22} />
           </button>
         </div>
