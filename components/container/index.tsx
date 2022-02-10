5e8a2a19a2cac0074cb245024897e7ab377903b3
riancarlosdev <riancontatoprofissional@gmail.com>
2022-02-10 08:17:57 -0300

    atualizando container

--- components/container/index.tsx
+++ components/container/index.tsx
@@ -2,13 +2,11 @@ import { container_type } from "../../types"
 
 
 export default function Container_component({children, background}:container_type):JSX.Element {
-
   const classNamePrime = `w-full ${background}`
-  const classNameInter = `max-w-`
 
   return (
     <div className={classNamePrime}>
-      <div className="max-w-7xl m-auto">
+      <div className="max-w-7xl m-auto px-4">
         {children}
       </div>
     </div>
