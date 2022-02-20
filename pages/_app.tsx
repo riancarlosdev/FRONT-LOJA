46dd973dc404d4eebdd9f46e72b404b87b2ed735
riancarlosdev <riancontatoprofissional@gmail.com>
2022-02-20 17:40:33 -0300

    att

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,6 @@ export default function MyApp({ Component, pageProps }:any) {
     <ProviderRedux>
       <Head>
         <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=optional" rel="stylesheet" />
