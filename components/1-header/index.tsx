2e92beffe61aeb5418e890be5847e0e414eadacd
riancarlosdev <riancontatoprofissional@gmail.com>
2022-02-21 09:23:32 -0300

    update redux

--- components/1-header/index.tsx
+++ components/1-header/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { redux_type } from "../../types";
 import Logo1_component from "../temp/logos/1";
 import BagCarrinho_component from "./bag/bag-carrinho";
 import InputSearch_component from "./input-search";
@@ -8,7 +9,7 @@ export default function Header_component():JSX.Element {
 
   const [ focus, setFocus ] = useState<boolean>(false)
 
-  const ActualWidth = useSelector(state => state)
+  const ActualWidth = useSelector((state:redux_type) => state.screen)
 
   return (
     <div className="duration-500 transition-all h-20 flex items-center justify-between space-x-5">
